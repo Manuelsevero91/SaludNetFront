a9e2e77c05710316fa0c3b17648749be1b2c2982
Karina Desposito <kvdesposito@gmail.com>
2024-05-26 13:50:17 -0300

    Componente List nuevo

--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -1,22 +1,18 @@
 import { Link } from "react-router-dom";
 import logoSN from "../assets/logosaludnet.png";
 import { useAuth } from "./UserContext";
-
 function NavBar({ showButtons }) {
   const { isLoggedIn, username, handleLogout } = useAuth();
-
   const handleLogoutClick = () => {
     handleLogout();
   };
-
   return (
     <>
       <div className="nav-container">
         <nav>
           <ul className="nav-links">
-            <li><Link to="/"><img id="logoSN" src={logoSN} alt="Logo" /></Link></li
-
-            <li><Link to="/contacto">Contacto</L
+            <li><Link to="/"><img id="logoSN" src={logoSN} alt="Logo" /></Link></li>
+            <li><Link to="/contacto">Contacto</Link></li>
             {isLoggedIn ? (
               <>
                 <Link to="/" onClick={handleLogoutClick}>
@@ -36,10 +32,4 @@ function NavBar({ showButtons }) {
     </>
   );
 }
-
 export default NavBar;
-
-
-
-
-
