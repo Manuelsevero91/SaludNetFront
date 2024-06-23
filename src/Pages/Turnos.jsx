d711adbf6ae6547de5cae19080b61fcc0ad011f4
imolinap9191 <imolinap91@gmail.com>
2024-06-23 20:07:34 -0300

    configuración turnos desde front + componente Spinner

--- src/Pages/Turnos.jsx
+++ src/Pages/Turnos.jsx
@@ -1,15 +1,21 @@
-import React, { useEffect, useState } from "react";
-import NavBar from "../Componentes/NavBar";
-
+import React, { useState, useEffect } from 'react';
+import Spinner from '../Componentes/Spinner';
+import Swal from 'sweetalert2';
 const ShiffSelection = () => {
+  const [patientData, setPatientData] = useState({
+    fullName: '',
+    dni: '',
+    mail: '',
+    phone: '',
+    address: '',
+    birthday: '',
+  });
+
+  const [doctorId, setDoctorId] = useState(null);
+  const [scheduleId, setScheduleId] = useState(null);
+  const [schedules, setSchedules] = useState([]);
   const [doctors, setDoctors] = useState([]);
   const [loadingDoctors, setLoadingDoctors] = useState(true);
-  const [schedules, setSchedules] = useState([]);
-  const [loadingSchedules, setLoadingSchedules] = useState(false);
-  const [specialities, setSpecialities] = useState([]);
-  const [searchName, setSearchName] = useState("");
-  const [searchSpeciality, setSearchSpeciality] = useState("");
-  const [selectedDoctor, setSelectedDoctor] = useState(null);
 
   useEffect(() => {
     const fetchDoctors = async () => {
@@ -20,155 +26,168 @@ const ShiffSelection = () => {
         }
         const result = await response.json();
         setDoctors(result.data);
-        setLoadingDoctors(false);
+        setTimeout(() => {
+          setLoadingDoctors(false);
+        }, 2000);
       } catch (error) {
-        console.error("Error fetching doctors:", error);
         setLoadingDoctors(false);
-      }
+      } 
     };
-
-    const fetchSpecialities = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/speciality");
-        if (!response.ok) {
-          throw new Error(`Error fetching specialities: ${response.status}`);
-        }
-        const responseData = await response.json();
-        setSpecialities(responseData.data);
-      } catch (error) {
-        console.error("Error fetching specialities:", error);
-      }
-    };
-
     fetchDoctors();
-    fetchSpecialities();
   }, []);
 
-  const fetchSchedule = async (doctorId) => {
-    try {
-      setLoadingSchedules(true);
-      const response = await fetch(`http://localhost:3000/schedules/by-doctor/${doctorId}`);
-      if (!response.ok) {
-        throw new Error(`Error fetching schedules: ${response.status}`);
+  useEffect(() => {
+    const fetchSchedules = async () => {
+      if (doctorId) {
+        try {
+          const response = await fetch(`http://localhost:3000/schedules/by-doctor/${doctorId}`);
+          if (!response.ok) {
+            throw new Error(`Error fetching schedules: ${response.status}`);
+          }
+          const data = await response.json();
+          setSchedules(data.data); 
+        } catch (error) {
+          setSchedules([]); 
+        }
+      } else {
+        setSchedules([]); 
       }
-      const result = await response.json();
-      setSchedules(result.data);
-      setLoadingSchedules(false);
+    };
+    fetchSchedules();
+  }, [doctorId]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setPatientData({
+      ...patientData,
+      [name]: value,
+    });
+  };
+
+  const handleDoctorChange = (e) => {
+    setDoctorId(e.target.value);
+  };
+
+  const handleScheduleChange = (e) => {
+    setScheduleId(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+    
+      const patientResponse = await fetch('http://localhost:3000/patients', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(patientData),
+      });
+
+      if (!patientResponse.ok) {
+        throw new Error('Error creating patient');
+      }
+
+      const patientResult = await patientResponse.json();
+      const patientId = patientResult.data.id;
+
+
+      const shiffResponse = await fetch('http://localhost:3000/shiff', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          idSchedule: scheduleId,
+          idPatient: patientId,
+        }),
+      });
+
+      if (!shiffResponse.ok) {
+        throw new Error('Error taking shiff');
+      }
+Swal.fire({text:"Turno reservado con éxito",icon:"success"})
     } catch (error) {
-      console.error("Error fetching schedules:", error);
-      setLoadingSchedules(false);
+
+      Swal.fire({text:'Hubo un error al reservar el turno',icon:"error"})
     }
   };
 
-  const handleDoctorSelect = (doctor) => {
-    setSelectedDoctor(doctor);
-    fetchSchedule(doctor.id);
-  };
 
-  const filteredDoctors = doctors.filter((doctor) => {
-    const fullNameMatch = doctor.fullName
-      .toLowerCase()
-      .includes(searchName.toLowerCase());
-    const specialityMatch = doctor.speciality.name
-      .toLowerCase()
-      .includes(searchSpeciality.toLowerCase());
-    return fullNameMatch && specialityMatch;
-  });
 
   return (
     <>
-      <NavBar showLinks={false} />
-      <div className="barra-superior">
-        <h2 className="titulo-section">
-          Seleccionar turnos disponibles
-        </h2>
-      </div>
-      <div className="search-bar">
-        <label className="search" htmlFor="searchName">
-          Nombre:
-        </label>
-        <input
-          type="text"
-          id="searchName"
-          value={searchName}
-          onChange={(e) => setSearchName(e.target.value)}
-        />
-        <label className="search" htmlFor="searchSpeciality">
-          Especialidad:
-        </label>
-        <select
-          id="searchSpeciality"
-          value={searchSpeciality}
-          onChange={(e) => setSearchSpeciality(e.target.value)}
-        >
-          <option value="">Todas</option>
-          {specialities.map((speciality) => (
-            <option key={speciality.id} value={speciality.name}>
-              {speciality.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="tableContainer">
-        <table>
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Especialidad</th>
-              <th>Licencia</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {loadingDoctors ? (
-              <tr>
-                <td colSpan="4">Cargando doctores...</td>
-              </tr>
-            ) : (
-              filteredDoctors.map((doctor) => (
-                <tr
-                  key={doctor.id}
-                  className={doctor.deleted ? "deleted-doctor" : ""}
-                >
-                  <td>{doctor.fullName}</td>
-                  <td>{doctor.speciality.name}</td>
-                  <td>{doctor.license}</td>
-                  <td>
-                    {!doctor.deleted && (
-                      <button
-                        className="turnos-disponibles"
-                        onClick={() => handleDoctorSelect(doctor)}
-                      >
-                        Turnos disponibles
-                      </button>
-                    )}
-                  </td>
-                </tr>
-              ))
-            )}
-          </tbody>
-        </table>
-      </div>
-      {selectedDoctor && (
-        <div className="scheduleList">
-          <h3>Turnos disponibles para {selectedDoctor.fullName}</h3>
-          {loadingSchedules ? (
-            <p>Cargando turnos...</p>
-          ) : (
-            <ul>
-              {schedules.length > 0 ? (
-                schedules.map((schedule) => (
-                  <li key={schedule.idSchedule}>
-                    {schedule.day} - {schedule.start_Time} a {schedule.end_Time}
-                  </li>
-                ))
-              ) : (
-                <p>No hay turnos disponibles</p>
-              )}
-            </ul>
-          )}
-        </div>
-      )}
+    <Spinner loading={loadingDoctors} />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="fullName"
+        value={patientData.fullName}
+        onChange={handleInputChange}
+        placeholder="Nombre"
+        required
+      />
+      <input
+        type="text"
+        name="dni"
+        value={patientData.dni}
+        onChange={handleInputChange}
+        placeholder="DNI"
+        required
+      />
+      <input
+        type="email"
+        name="mail"
+        value={patientData.mail}
+        onChange={handleInputChange}
+        placeholder="MAIL"
+        required
+      />
+       <input
+        type="text"
+        name="phone"
+        value={patientData.phone}
+        onChange={handleInputChange}
+        placeholder="PHONE"
+        required
+      />
+      <input
+        type="text"
+        name="address"
+        value={patientData.address}
+        onChange={handleInputChange}
+        placeholder="ADDRESS"
+        required
+      />
+      <input
+        type="text"
+        name="birthday"
+        value={patientData.birthday}
+        onChange={handleInputChange}
+        placeholder="BIRTHDAY"
+        required
+      />
+      <select onChange={handleDoctorChange} required>
+        <option value="">Seleccione un doctor</option>
+        {doctors.map((doctor) => (
+          <option key={doctor.id} value={doctor.id}>
+            {doctor.fullName} - {doctor.speciality.name}
+          </option>
+        ))}
+      </select>
+
+      <select onChange={handleScheduleChange} required>
+        <option value="">Seleccione un horario</option>
+        {schedules.map((schedule) => (
+          <option key={schedule.idSchedule} value={schedule.idSchedule}>
+            {schedule.day} - {schedule.start_Time} a {schedule.end_Time}
+          </option>
+        ))}
+      </select>
+
+      <button type="submit">Reservar Turno</button>
+    </form>
     </>
   );
 };
