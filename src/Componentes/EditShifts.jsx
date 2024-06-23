8e524401daef9dad885bdfca17a5d8b9c960a09c
imolinap9191 <imolinap91@gmail.com>
2024-06-23 20:30:51 -0300

    se crea reservar turnos + componente Spinner

--- src/Componentes/EditShifts.jsx
+++ src/Componentes/EditShifts.jsx
@@ -1,13 +1,30 @@
+/*import React, { useState, useEffect } from 'react';
+import Spinner from '../Componentes/Spinner';
+import Swal from 'sweetalert2';
 
+const EditShiffs = () => {
+    const [shiffs,setShiffs]=useState([]);
+    const [loading,setLoading]=useState(true);
+    useEffect(()=>{
+        const fetchShiffs = async ()=>{
+            try {
+                const listShiffs = await fetch("http://localhost:3000/shiff");
+                const response = await listShiffs.json()
+                setShiffs(response.data)
+                setTimeout(()=>{
+                    setLoading(false);
+                },2000);
 
+            } catch (error) {
+                setLoading(false)
+            }
+        }
+        fetchShiffs();
+    },[])
 
-
-
-
-const EditShifts = () => {
     return (
-    <h2>EditShifts</h2>
+    <h2>EditShiffs</h2>
     )
 }
 
-export default EditShifts;
+export default EditShiffs;*/
